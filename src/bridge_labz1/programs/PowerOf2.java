dee44a7d876643a5fe2d32b1dfc756ced159553d
gautam <gautamk173@gmail.com>
2017-02-06 18:53:58 +0530

    PowerOf206.01

--- src/bridge_labz1/programs/PowerOf2.java
+++ src/bridge_labz1/programs/PowerOf2.java
@@ -4,13 +4,16 @@ public class PowerOf2 {
 	public static void main(String[] args) {
 		int num=Integer.parseInt(args[0]);
 		int turn =1;
-	
+		if(num>=0 && num<31){
 		for(int i=1;i<=num;i++){
 			turn = turn*2;
 			
 			System.out.println(2+"^"+i+" = "+ turn );
 			
 			}
+		}
+		else 
+			System.out.println("invalid input try again..!!!");
 	}
 	
 }
