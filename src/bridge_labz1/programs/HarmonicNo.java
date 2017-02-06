136dba48fc51137f42104eaafb9397e7c7227a46
gautam <gautamk173@gmail.com>
2017-02-06 18:56:16 +0530

    HarmonicNo05.01

--- src/bridge_labz1/programs/HarmonicNo.java
+++ src/bridge_labz1/programs/HarmonicNo.java
@@ -9,12 +9,15 @@ public class HarmonicNo {
 		Scanner scn=new Scanner(System.in);
 		int count=scn.nextInt();
 		scn.close();
+		if(count>0){
 		double har=0.0;
 		for(int i=1;i<=count;i++){
 			har=har+((double)1/(double)i);
 			
 		}
-		System.out.println("Harmonic no. for"+count+" = "+har);
+		System.out.println("Harmonic no. for "+count+" = "+har);
+		}
+		else System.out.println("input can't be 0 or nigative");
 	}
 
 }
