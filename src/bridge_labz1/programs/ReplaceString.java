8d71b7ad12cb3e1abd8d713771fae45f8f810d50
gautam <gautamk173@gmail.com>
2017-02-06 12:48:06 +0530

    replace_sring 01

---
+++ src/bridge_labz1/programs/ReplaceString.java
@@ -0,0 +1,21 @@
+package bridge_labz1.programs;
+
+import java.util.Scanner;
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		Scanner scn =new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String s=scn.nextLine();
+		
+			while(s.length()<=3){	
+				System.out.println("user name too short 'Re-enter'");
+					s=scn.nextLine();
+				}
+			
+			System.out.println("hello "+s+" how are you?");
+		
+		}
+
+}
