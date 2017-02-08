a56ef075eac2932a8ed120c1a8b399026056a2de
gautam <gautamk173@gmail.com>
2017-02-08 10:16:19 +0530

    yet to work on 2DArray

--- src/bridge_labz1/programs/TwoDArray.java
+++ src/bridge_labz1/programs/TwoDArray.java
@@ -2,26 +2,39 @@ package bridge_labz1.programs;
 
 import java.io.*;
 import java.util.Scanner;
-
 public class TwoDArray {
 	static Scanner scn =new Scanner(System.in);
-	static int [][]arr;
-	public static void main(String[] args) {
+	static int [][]arrInt;
+	static double [][] arrDouble;
+	public static void main(String[] args)  {
 		
 		System.out.println("press 1 for integer Array[] :");
 		System.out.println("press 2 for double Array[] :");
 		System.out.println("press 3 for boolean Array[] :");
 		int in=scn.nextInt();
+		System.out.println("enter row");
+		int row=scn.nextInt();
+		System.out.println("enter coll");
+		int coll=scn.nextInt();
 		switch(in){
 		case 1:	
-			getIntegerArray();
-			System.out.println();
 			
-				diaplay();
+			try {
+				getIntegerArray(row, coll);
+				diaplay(row ,coll);
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+					
 			
 			break;
 		case 2:
-			getDoubleArray();
+			try {
+				getDoubleArray(row ,coll);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 			break;
 		case 3:
 			getBooleanArray();
@@ -30,39 +43,61 @@ public class TwoDArray {
 			System.out.println("invalid input");
 		}
 	}
-
-	private static void diaplay() throws FileNotFoundException {
-		//OutputStream output = new FileOutputStream("data/data.bin");
-		FileOutputStream fs=new FileOutputStream("txt/text");
-
+	//Display method
+	private static void diaplay(int row,int coll) throws FileNotFoundException {
 		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < coll; j++) {
+				
+				int p=arrInt[i][j];
+			}
+		}
 	}
 
 	private static void getBooleanArray() {
 		
 	}
 
-	private static void getDoubleArray() {
-		
-	}
-
-	private static void getIntegerArray() throws IOException {
-		File file = new File("txt/text");
-		OutputStreamWriter os = new OutputStreamWriter(new FileOutputStream(file));
-		System.out.println("enter row");
-		int row=scn.nextInt();
-		System.out.println("enter coll");
-		int coll=scn.nextInt();
-		arr=new int[row][coll];
+	private static void getDoubleArray(int row,int coll) throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter("/home/bridgeit/Desktop/Gauti_java/Start/txt/double.txt");
+		arrDouble=new double[row][coll];
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < coll; j++) {
 				System.out.println("enter the integer values");
-				arr[i][j]=scn.nextInt();
-				os.write(arr[i][j]);
+				arrDouble[i][j]=scn.nextDouble();
 			}
-			
 		}
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < coll; j++) {
+				pw.flush();
+				double p=arrDouble[i][j];
+				pw.print(p+" ");
+			}
+			pw.println();
+		}
+		pw.close();
+	}
+
+	
+	private static void getIntegerArray(int row,int coll) throws FileNotFoundException {
+
+        PrintWriter pw = new PrintWriter("/home/bridgeit/Desktop/Gauti_java/Start/txt/int.txt");
+		arrInt=new int[row][coll];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < coll; j++) {
+				System.out.println("enter the integer values");
+				arrInt[i][j]=scn.nextInt();
+			}
+		}
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < coll; j++) {
+				pw.flush();
+				int p=arrInt[i][j];
+				pw.print(p+" ");
+			}
+			pw.println();
+		}
+		pw.close();
 	}
 	
-
 }
