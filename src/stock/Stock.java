3e9250f75b931efcad1fe9d35acc5d2730037720
gautam <gautamk173@gmail.com>
2017-02-09 12:34:53 +0530

    Stock Done

---
+++ src/stock/Stock.java
@@ -0,0 +1,32 @@
+package stock;
+
+public class Stock {
+	int noOfShare;
+	int noOfStock;
+	String stockName;
+	double sharePrice;
+	static double totalStock;
+	double stockPrice;
+	public Stock(String s,int no,Double share) {
+		stockName=s;
+		noOfShare=no;
+		sharePrice=share;
+	}
+	public Stock() {
+		
+	}
+	
+		double calculateStock() {
+			double dec=((10*sharePrice)/100);
+			stockPrice=dec*noOfShare;
+			totalStock+=stockPrice;
+			return stockPrice;
+		}
+
+	@Override
+	public String toString() {
+		
+		return "["+stockName+"  "+noOfShare+"  "+sharePrice+"  "+stockPrice+"]";
+	}
+	
+}
---
+++ src/stock/StockPortfolio.java
@@ -0,0 +1,38 @@
+package stock;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class StockPortfolio {
+	
+	public static void main(String[] args) {
+	Scanner scn=new Scanner(System.in);
+	System.out.println("No. of Stock");
+	int noOfStock =scn.nextInt();
+	Queue<Stock> q=new LinkedList<Stock>();
+
+	for(int i=1;i<=noOfStock;i++){
+		
+		System.out.println(i+"."+"Enter StockName: ");
+		String s=scn.next();
+		System.out.println(i+"."+"No of Share: ");
+		int no =scn.nextInt();
+		System.out.println(i+"."+"Share price: ");
+		double price=scn.nextDouble();
+		Stock obj=new Stock(s,no,price);
+		obj.calculateStock();
+		q.add(obj);
+	}
+	System.out.println("StockName | NoOShare | Shareprice | Stock");
+	for (int i = 0; i < noOfStock; i++) {
+		System.out.println(q.poll());
+	}
+	Stock s=new Stock();
+	System.out.println();
+	System.out.println("........................TotalStock");
+	System.out.println("........................["+s.totalStock+"]");
+	
+	
+	}
+}
