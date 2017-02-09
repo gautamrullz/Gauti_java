4d6ba51002d3af3f85a1feb7a109c52ead2c8af4
gautam <gautamk173@gmail.com>
2017-02-09 17:59:17 +0530

    binarySearchWord016

---
+++ src/binarySearchWord/BinarySearchWord.java
@@ -0,0 +1,63 @@
+package binarySearchWord;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BinarySearchWord {
+	public static void main(String[] args) throws IOException {
+		Scanner scn =new Scanner(System.in);
+		String s="";	
+		FileReader f=new FileReader("/home/bridgeit/Desktop/Gauti_java/Start/abc.txt");
+		BufferedReader b= new BufferedReader(f);
+		s=b.readLine();
+		b.close();
+		System.out.println(s);
+		String[] str=s.split(",");
+		str=sort(str);
+		System.out.println();
+		System.out.println("search a string");
+		String s1=scn.next();
+		int first=0;
+		int last=str.length-1;
+		int mid;
+		while(first<=last){
+			mid=(first+last)/2;
+			if(s1.compareTo(str[mid])>0){
+				first=mid+1;
+			}
+			else if(s1.compareTo(str[mid])<0){
+				last=mid-1;
+			}
+			else{
+				System.out.println();
+				System.out.println("found the string....!!!!!   :"+str[mid]);
+				return;
+			}
+		}
+		System.out.println();
+		System.out.println("could not found the string");
+	}
+
+	private static String[] sort(String[] str) {
+		
+		for (int i = 0; i < str.length-1; i++) {
+			for (int j = 0; j < str.length-1-i; j++) {
+				if(str[j].compareTo(str[j+1])>0){
+					String temp= str[j];
+					str[j]=str[j+1];
+					str[j+1]=temp;
+				}
+			}
+			
+		}
+		for(String ele :str){
+			System.out.println(ele);
+		}
+		
+		return str;
+	}
+
+}
