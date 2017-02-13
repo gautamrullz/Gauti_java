364069858688dba0dc02a2fd29ac4b814b691487
gautam <gautamk173@gmail.com>
2017-02-13 13:25:57 +0530

    improved

--- src/balance/BalancedParentheses.java
+++ src/balance/BalancedParentheses.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class BalancedParentheses {
 
 	public static void main(String[] args) {
+		System.out.println("Enter a Expression to check its balanced or not");
 		Scanner scn=new Scanner(System.in);
 		String s=scn.nextLine();
 		scn.close();
