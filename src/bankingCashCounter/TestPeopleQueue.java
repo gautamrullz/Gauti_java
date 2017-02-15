95e7265474e46f1bac26ae6f83f32d58a3aeacfd
gautam <gautamk173@gmail.com>
2017-02-15 10:35:59 +0530

    update

--- src/bankingCashCounter/TestPeopleQueue.java
+++ src/bankingCashCounter/TestPeopleQueue.java
@@ -20,38 +20,36 @@ public class TestPeopleQueue {
 				System.out.println("4. new person came join the queue");
 				double d;
 				int ch=scn.nextInt();
-			if(ch==1){
-				System.out.println("enter amount you want to Deposit");
-				d=scn.nextDouble();
-				p.deposit(d);
-			}
-			else if(ch==2){	
-				System.out.println("enter amount you want to Withdraw");
-				d=scn.nextDouble();
-				p.withdraw(d);
-			}
+				if(ch==1){
+					System.out.println("enter amount you want to Deposit");
+					d=scn.nextDouble();
+					p.deposit(d);
+				}
+				else if(ch==2){	
+					System.out.println("enter amount you want to Withdraw");
+					d=scn.nextDouble();
+					p.withdraw(d);
+				}
+				
+				else if(ch==3){
+					System.out.println(p.balance());	
+				}
 			
-			else if(ch==3){
-				System.out.println(p.balance());	
-			}
+				else if(ch==4){
+					q.enqueue(new People());
+					System.out.println("person in queue: "+q.size());
+				}
 			
-			else if(ch==4){
-				q.enqueue(new People());
-				System.out.println("person in queue: "+q.size());
-			}
+				else{
+					System.out.println("invalid input");
+				}
 			
-			else{
-				System.out.println("invalid input");
-			}
-			
-			System.out.println("press 'Y' to continue else 'N'");
-			y=scn.next();
+				System.out.println("press 'Y' to continue else 'N'");
+				y=scn.next();
 			}
 		}
-			scn.close();
-		System.out.println("no person in Queue");
-			
-			
+		scn.close();
+		System.out.println("no person in Queue");	
 	}
 		
 }
